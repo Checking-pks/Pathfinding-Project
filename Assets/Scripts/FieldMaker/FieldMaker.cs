148fba0caf17c1cb49ae08aeac6fbe4e5d11a7a9
Checking-pks <qkrrudtn23@naver.com>
2024-02-03 16:52:18 +0900

    feat: 필드 크기 변경시 새롭게 필드가 생성되도록 변경

--- Assets/Scripts/FieldMaker/FieldMaker.cs
+++ Assets/Scripts/FieldMaker/FieldMaker.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 
 public class FieldMaker : MonoBehaviour
 {
-    protected bool[,] fieldList;
-    protected GameObject[,] blockList;
+    public bool[,] fieldList;
+    public GameObject[,] blockList;
     protected string floorName = "Floor";
     protected GameObject floor;
+    protected string playerName = "Player";
+    protected GameObject player;
     protected GameObject parent;
 
     protected float m_fieldSizeRow, m_fieldSizeCol;
@@ -18,16 +20,34 @@ public class FieldMaker : MonoBehaviour
     public GameObject wall;
     [Range(10, 100)]
     public int m_row, m_col;
+    private int lastRow=0, lastCol=0;
     public bool m_makeField = true;
     public KeyCode m_key = KeyCode.F;
 
-    private void Awake()
+    private void Start()
     {
         Init();
     }
 
+    private void OnEnable()
+    {
+        DisableAnotherFieldMaker();
+        SetField();
+    }
+
+    private void OnDisable()
+    {
+        DisableField();
+    }
+
     private void Update()
     {
+        if (lastRow != m_row || lastCol != m_col)
+        {
+            lastRow = m_row; lastCol = m_col;
+            m_makeField = true;
+        }
+
         if (Input.GetKey(m_key))
             m_makeField = true;
 
@@ -41,19 +61,10 @@ public class FieldMaker : MonoBehaviour
 
     virtual protected void Init()
     {
-        floor = transform.Find(floorName)?.gameObject;
+        floor  = transform.Find(floorName)? .gameObject;
+        player = transform.Find(playerName)?.gameObject;
 
-        if (floor == null)
-        {
-            Debug.LogError($"이 오브젝트의 하위 {floorName}을 찾을 수 없습니다.");
-            return;
-        }
-
-        if (wall == null)
-        {
-            Debug.LogError($"Wall Prefab을 찾을 수 없습니다.");
-            return;
-        }
+        DataInspection();
 
         fieldList = new bool[100, 100];
         blockList = new GameObject[100, 100];
@@ -69,8 +80,54 @@ public class FieldMaker : MonoBehaviour
 
     virtual protected void FieldMake() { }
 
+    private bool DataInspection()
+    {
+        if (floor == null)
+        {
+            Debug.LogError($"이 오브젝트의 하위 {floorName}을 찾을 수 없습니다.");
+            return false;
+        }
+
+        if (player == null)
+        {
+            Debug.LogError($"이 오브젝트의 하위 {playerName}을 찾을 수 없습니다.");
+            return false;
+        }
+
+        if (wall == null)
+        {
+            Debug.LogError($"Wall Prefab을 찾을 수 없습니다.");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void DisableAnotherFieldMaker()
+    {
+        FieldMaker[] fieldMakers = GetComponents<FieldMaker>();
+        for (int i = 0;i < fieldMakers.Length;i++)
+        {
+            if (fieldMakers[i] != this)
+                fieldMakers[i].enabled = false;
+        }
+    }
+
     virtual protected void SetField()
     {
+        if (!DataInspection()) return;
+
+        // 플레이어 배치
+        player.transform.position =
+            Vector3.right * ((1 + .5f) * m_blockSizeRow - m_fieldSizeRow) +
+            Vector3.forward * ((m_col - 2 + .5f) * m_blockSizeCol - m_fieldSizeCol);
+
+        player.transform.localScale =
+            Vector3.right * m_blockSizeRow * .75f +
+            Vector3.up +
+            Vector3.forward * m_blockSizeCol * .75f;
+
+        // 필드 배치
         for (int i = 0; i < 100; i++)
         {
             for (int j = 0; j < 100; j++)
@@ -89,6 +146,17 @@ public class FieldMaker : MonoBehaviour
                     Vector3.right * m_blockSizeRow +
                     Vector3.up +
                     Vector3.forward * m_blockSizeCol;
+            }
+        }
+    }
+
+    virtual protected void DisableField()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            for (int j = 0; j < 100; j++)
+            {
+                blockList[i, j].SetActive(false);
             }
         }
     }
